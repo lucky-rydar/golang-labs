84172589deeb0a64ca801c34434f9744f008d453
Mykhailo Didur <mikhaildidur2003@gmail.com>
2023-03-30 20:44:09 +0300

    Implement admin verification for dormitory stats getting
    Signed-off-by: Mykhailo Didur <mikhaildidur2003@gmail.com>

--- handlers/DormitoryLoadController.go
+++ handlers/DormitoryLoadController.go
@@ -7,9 +7,25 @@ import (
 	"github.com/it-02/dormitory/service"
 )
 
+type GetDormStatsRequest struct {
+	UUID string `json:"uuid"`
+}
+
 func GetDormitoryLoadHandler(w http.ResponseWriter, r *http.Request) {
-	dormitoryLoad := service.GetDormitoryLoad()
-	err := json.NewEncoder(w).Encode(dormitoryLoad)
+	var request GetDormStatsRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	dormitoryLoad, err := service.GetDormitoryLoad(request.UUID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(dormitoryLoad)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
--- service/DormitoryLoadService.go
+++ service/DormitoryLoadService.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"fmt"
+
 	"github.com/it-02/dormitory/repository"
 	"github.com/it-02/dormitory/db"
 )
@@ -13,12 +15,16 @@ type DormitoryLoad struct {
 	OccupiedFemalePlaces []db.Place
 }
 
-func GetDormitoryLoad() DormitoryLoad {
+func GetDormitoryLoad(uuid string) (DormitoryLoad, error) {
+	if !IsUserAdmin(uuid) {
+		return DormitoryLoad{}, fmt.Errorf("User is not admin")
+	}
+
 	var dormitoryLoad DormitoryLoad
 	dormitoryLoad.TotalPlacesAmount = len(repository.GetPlaces())
 	dormitoryLoad.FreeMalePlaces = repository.GetPlacesByParams(true, true)
 	dormitoryLoad.FreeFemalePlaces = repository.GetPlacesByParams(false, true)
 	dormitoryLoad.OccupiedMalePlaces = repository.GetPlacesByParams(true, false)
 	dormitoryLoad.OccupiedFemalePlaces = repository.GetPlacesByParams(false, false)
-	return dormitoryLoad
+	return dormitoryLoad, nil
 }
